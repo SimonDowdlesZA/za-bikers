a26510856a8d644c700f1e79feb3a4d3f88eb7a5
SimonDowdlesZA <simondowdles@gmail.com>
2025-03-13 10:31:54 +0200

    Add no follow and no index links.

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,9 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>ZA Bikers is Changing Gears</title>
+        <title>ZA Bikers</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="robots" content="noindex,nofollow,noarchive" />
       </Head>
 
       <main>
